7d22a3cbb97e91fb43ec11808b7c540a5ffd0f9e
shehanjamal12 <shehan@agile.financial>
2020-08-06 15:53:20 +0530

    comments added

--- api.go
+++ api.go
@@ -8,16 +8,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//Items struct
 type Items struct {
-	ItemName     string `json:"itemName"`
+	ItemName     string `json:"itemName"` //json part so that in postman it comes as simple i not capital
 	ItemQuantity int    `json:"itemQuantity"`
 }
 
-var itemList []Items = []Items{}
+var itemList []Items = []Items{} //slice used to hold all the items which will be added dleted or updated
 
 func main() {
 	route := mux.NewRouter()
 
+	//routes used in postman tomake request or get responses
 	route.HandleFunc("/addItem", addItem).Methods("POST")
 	route.HandleFunc("/viewAllItem", viewAllItem).Methods("GET")
 	route.HandleFunc("/viewItem/{id}", viewItem).Methods("GET")
@@ -26,15 +28,15 @@ func main() {
 	route.HandleFunc("/delItembyName/{name}", delItembyName).Methods("DELETE")
 	route.HandleFunc("/updateItembyId/{id}", updateItembyID).Methods("PUT")
 	route.HandleFunc("/updateItembyName/{name}", updateItembyName).Methods("PUT")
-	http.ListenAndServe(":5000", route)
+	http.ListenAndServe(":5000", route) //port used to run api
 
 }
 func delItembyName(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json") //makes sure all content in the reponse is of json type
 	var name = mux.Vars(r)["name"]
 
 	var i int = 0
-	var length = len(itemList)
+	var length = len(itemList) //get length of list
 	for _, item := range itemList {
 		if item.ItemName == name {
 			itemList = append(itemList[:i], itemList[i+1:]...)
@@ -46,6 +48,7 @@ func delItembyName(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("Error: Could Not Find Item"))
 		}
 	}
+	//response of the fucntion
 	json.NewEncoder(w).Encode(itemList)
 }
 func viewbyName(w http.ResponseWriter, r *http.Request) {
@@ -70,22 +73,24 @@ func viewbyName(w http.ResponseWriter, r *http.Request) {
 
 func delItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var idparam = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idparam)
-
+	var idparam = mux.Vars(r)["id"]  //get the id from postman request url
+	id, err := strconv.Atoi(idparam) //vonversion from string to integer
+	//validation
 	if err != nil {
 		w.Write([]byte("Error: Id Could not be converted"))
 	}
 	if id > len(itemList) {
 		w.Write([]byte("Error: Id Could Not Be found"))
 	}
+	//deleting and shifting the slice so that it will be ordered again correctly
 	itemList = append(itemList[:id], itemList[id+1:]...)
 	json.NewEncoder(w).Encode(itemList)
 }
 func addItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var item Items
-	json.NewDecoder(r.Body).Decode(&item)
+	//used to get the request body and sets it to the variable
+	json.NewDecoder(r.Body).Decode(&item) //pointer is used here so that it will diretcly address the variable
 	itemList = append(itemList, item)
 
 	json.NewEncoder(w).Encode(itemList)
